03b043ae1732c806f6c4cb91238e803bda79de7d
Jarl Friis <jarl@softace.dk>
2018-11-18 08:07:46 +0100

    Various curves

--- beating_heart.py
+++ beating_heart.py
@@ -1,28 +1,18 @@
 from microbit import *
 import math
 
-boat = Image("05050:"
-             "05050:"
-             "05050:"
-             "99999:"
-             "09990")
-#display.show(boat)
-display.clear()
+steps = 500
 
 hollow_heart = Image.HEART - Image.HEART_SMALL
-steps = 500
-i = 0
-
-
 def show_image(fraction):
-    level = math.floor(4.5+0.5+fraction*4.5)
-    display.show(hollow_heart+Image.HEART_SMALL*(0.5+0.5*fraction))
-#    display.set_pixel(0, 0, level)
-#    display.set_pixel(0, 1, level)
-#    display.set_pixel(1, 0, level)
-#    display.set_pixel(1, 1, level)
+    display.show(hollow_heart + Image.HEART_SMALL*(fraction))
     
+i = 0
+display.clear()
 while True:
-    fract = math.cos(i*2*math.pi/float(steps))
+    if(button_a.is_pressed()):
+        fract = (steps/2-abs(i-steps/2))/(steps/2)
+    else:
+        fract = 0.5+0.5*math.cos(i*2*math.pi/steps)
     show_image(fract)
-    i = (i+1)%steps
+    i = (i+1) % steps
